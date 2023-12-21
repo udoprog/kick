fe9149eacea09fa8b87240f4a6fb5487d36cb055
John-John Tedro <udoprog@tedro.se>
2023-12-21 21:31:26 +0100

    Fix date formatting

--- src/release/mod.rs
+++ src/release/mod.rs
@@ -208,7 +208,7 @@ impl Date {
 impl fmt::Display for Date {
     #[inline]
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}.{}.{}", self.year, self.month, self.day)
+        write!(f, "{}-{}-{}", self.year, self.month, self.day)
     }
 }
 
