5755ac48a22133be12ad48bd43cebc9ace8ce85f
John-John Tedro <udoprog@tedro.se>
2024-02-08 17:29:44 +0100

    Use hard-coded default version in branches

--- src/main.ts
+++ src/main.ts
@@ -1,46 +1,9 @@
 import * as core from '@actions/core';
 import * as tc from '@actions/tool-cache';
-import * as httpm from '@actions/http-client';
-
-const USER_AGENT = 'udoprog/kick-action';
 
 const IS_WINDOWS = process.platform === 'win32'
 const IS_MAC = process.platform === 'darwin'
 
-async function version(repo: string, key: string): Promise<string> {
-    const version = core.getInput(key);
-
-    if (version !== 'latest') {
-        return version;
-    }
-
-    core.info(`Searching the latest version of ${repo} ...`);
-
-    const http = new httpm.HttpClient(USER_AGENT, [], {
-        allowRetries: false
-    });
-
-    const response = await http.get(`https://api.github.com/repos/${repo}/releases/latest`);
-    const body = await response.readBody();
-
-    if (response.message.statusCode !== 200) {
-        throw new Error(`Failed to fetch latest version of ${repo}: ${response.message.statusCode}: ${body}`);
-    }
-
-    let data = JSON.parse(body);
-
-    if (!data.tag_name) {
-        throw new Error(`Failed to fetch latest version of ${repo}: No tag_name`);
-    }
-
-    return data.tag_name;
-}
-
-/**
- * Download and return the path to an executable kick tool.
- *
- * @param tag The tag to download.
- */
 async function download(tag: string): Promise<string> {
     let platform;
     let ext = 'tar.gz';
@@ -76,10 +39,10 @@ async function download(tag: string): Promise<string> {
 }
 
 async function innerMain() {
-    const tag = await version('udoprog/kick', 'version') || process.env.GITHUB_ACTION_REF;
+    const tag = await core.getInput("version");
 
     if (!tag) {
-        throw new Error("No version found or specified");
+        throw new Error("Could not determine version of kick to use");
     }
 
     core.info(`Downloading 'kick' from tag '${tag}'`);
